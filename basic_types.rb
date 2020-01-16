0e6711752de7b661913524b7265ffbf45eba014e
Konstantin <hippielovesflowers@gmail.com>
2020-01-16 19:24:20 +0300

    update basic_types.rb -> fix comments

--- basic_types.rb
+++ basic_types.rb
@@ -5,7 +5,9 @@ frady = "Frady"
 fradie = frady          # fradie[0] = 'B' --> frady == "Brady"
 fradie = frady.clone    # Клонирование в независимый объект
 fradie[0] = 'B'         # 'Brady'
+
 p frady != fradie
+
 
 #   Целые числа и числа с плавающей точкой
 #   - !Общая арифметика и приоритет к типу float
@@ -13,7 +15,9 @@ p frady != fradie
 int_num = 12
 sec_int_num = int_num / 2   # 6
 float_num = int_num / 2.0   # 6.0
+
 p sec_int_num == float_num
+
 
 #   Массивы
 #   - !Гетерогенность элементов [1, 2.34, 'a', 'String!'..]
@@ -21,20 +25,21 @@ p sec_int_num == float_num
 #   -- (-) удаление элементов
 #   -- (*n) умножение(дублирование) эелементов
 arr = [1, 1, 'a', 2, 3, 'Github', 72]            
-arr += [44]                            # добавляем елемент(ы) в массив
-sec_arr = arr + [5, 6]                # [1, 1, 'a', 2, 3, 'Github', 72, 44, 5, 6]   
-third_arr = sec_arr - [1, 3, 12]      # ['a', 2, 'Github', 72, 44, 5, 6]
-                                        # ...игнорирует [12]
+arr += [44]                             # добавляем елемент(ы) в массив
+sec_arr = arr + [5, 6]                  # [1, 1, 'a', 2, 3, 'Github', 72, 44, 5, 6]   
+third_arr = sec_arr - [1, 3, 12]        # ['a', 2, 'Github', 72, 44, 5, 6] ...игнорирует [12]
+
 p [arr[0], arr, sec_arr, third_arr, [1, 2.3, 'Boom!'] * 2]
+
 
 #   Ассоциативные массивы
 #   - !Гетерогенность элементов и ключей {1=>"один", "два"=>2}
 #   -- (.delete) удаление элемента по ключу
 #   -- (.merge!) слияние объектов, (.merge) слияние в независимый объект
 hash = {1=>"one", "two"=>2}
-hash["three"] = 3.0                         # {1="one", "two"=>2, "three"=>3.0}
-                                            # ...равносильно hash.merge!({"three"=>3.0})
+hash["three"] = 3.0                         # {1="one", "two"=>2, "three"=>3.0} ...равносильно hash.merge!({"three"=>3.0})
 sec_hash = hash.merge({"four"=>"четыре"})
+
 p [hash, sec_hash, hash != sec_hash]
 
 #   Строки
@@ -42,7 +47,8 @@ p [hash, sec_hash, hash != sec_hash]
 #   -- Любой объект преобразуется в строку по контексту или с помощью (.to_s)
 name = 'Konstantin'
 age = 30
-welcome = "Hello, #{name}(#{age})" # "Hello, Konstantin(30)"
+welcome = "Hello, #{name}(#{age})"  # "Hello, Konstantin(30)"
+
 p [welcome, age, age.to_s]
 
 #   Диапазоны
@@ -50,4 +56,5 @@ p [welcome, age, age.to_s]
 #   -- (n..m) от n до m(включительно)
 #   -- (n...m) от n до m-1
 range_nums = 1..5
+
 p [range_nums, range_nums.to_a]
