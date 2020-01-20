4eaf04e70b82b74269c2272777eb2c7465c14ee8
Konstantin <bluesbaker@mail.ru>
2020-01-20 21:35:49 +0300

    add string_type.rb

---
+++ string_type.rb
@@ -0,0 +1,30 @@
+# Особенности работы с типом String 
+
+# Копия строки
+str = String.new("Bang!")
+
+# Форматирование строки - %
+# ...если символов форматирование несколько -- передается массив
+# Подробнее о форматировании строки -> Kernel#sprintf
+str = "%05d" % 12   # "00012"
+str = "%-2s: %08x" % ["ID", self.__id__]    # "ID:  0138651c"
+
+# Повторение строки - *
+str = "Bang!" * 3   # "Bang!Bang!Bang!"
+
+# Сцепление строки -- +, <<, concat...
+str = "Hell" + "o, "
+str << "Konstantin" 
+str << 33           # "Hello, Konstantin!"
+
+# Сравнение строк
+# ...или .casecmp для регистрозависимого сравнения
+str = "abcdef"
+result = str <=> "abcdef"   # 0
+result = str <=> "abcde"    # 1
+result = str <=> "abcdefg"  # -1
+result = str <=> "ABCDEF"   # 1
+
+# Сопоставление с шаблоном - =~, .match
+str = "cat o' 9 tails" =~ /\d/
+
