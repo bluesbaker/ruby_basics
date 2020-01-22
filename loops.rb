82caaade17278bb80c0a1d3c8008b39ae15fe2e1
Konstantin <bluesbaker@mail.ru>
2020-01-22 19:16:55 +0300

    add loops.rb

---
+++ loops.rb
@@ -0,0 +1,18 @@
+# Циклы и всякое с ними
+
+# while
+count = 5
+while count >= 0 do
+    count -= 1
+end
+
+# for in range or array...
+for item in 1..5 do
+    # 1, 2, 3, 4, 5
+end
+
+# метод .times как цикл
+5.times do
+    # ! * 5
+end
+
