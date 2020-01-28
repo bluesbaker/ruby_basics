430598d158b2c3882af6deb85b943ff467871c6e
Konstantin <bluesbaker@mail.ru>
2020-01-28 23:54:47 +0300

    delete averangenum.rb

--- examples/averagenum.rb
+++
@@ -1,9 +0,0 @@
-puts "Введите число 1:"
-num1 = gets.chomp.to_i
-puts "Введите число 2:"
-num2 = gets.chomp.to_i
-puts "Введите число 3:"
-num3 = gets.chomp.to_i
-
-average = (num1 + num2 + num3) / 3
-puts "Среднее арифметическое " + average.to_s
